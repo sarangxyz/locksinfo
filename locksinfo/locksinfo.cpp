d0310b61aed326d55eb510f871c3c2f1b2d9541b
Sarang Baheti <sarangbaheti@gmail.com>
2017-09-05 23:26:36 -0700

    a few more changes

--- locksinfo/locksinfo.cpp
+++ locksinfo/locksinfo.cpp
@@ -3,6 +3,7 @@
 
 #include <Windows.h>
 #include "DebugQuery.h"
+#include <set>
 #include <sstream>
 #include <tchar.h>
 #include <TlHelp32.h>
@@ -32,149 +33,217 @@ static bool wchar2long_ss(const WCHAR* str, DWORD* out)
     return !buffer.fail();
 }
 
+//---------------------------------------------------------------------------------------------------------------------------
+namespace
+{
+    class ScopedHandle
+    {
+    private:
+        HANDLE _handle;
+    public:
+        ScopedHandle(HANDLE hdl) : _handle(hdl)
+        {}
+
+        HANDLE getHandle() const
+        {
+            return _handle;
+        }
+
+        bool isValidHandle() const
+        {
+            return _handle != NULL && _handle != INVALID_HANDLE_VALUE;
+        }
+
+        ~ScopedHandle()
+        {
+            if (isValidHandle())
+            {
+                CloseHandle(_handle);
+                _handle = NULL;
+            }
+        }
+    };
+}
+
 
 //---------------------------------------------------------------------------------------------------------------------------
 static bool GetProcName(DWORD aPid, std::wstring* out)
 {
+    std::wstring procName;
     PROCESSENTRY32 processInfo;
     processInfo.dwSize = sizeof(processInfo);
-    HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-    if (processesSnapshot == INVALID_HANDLE_VALUE)
     {
-        return false;
-    }
+        ScopedHandle scopedProcessesSnapshot(CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL));
+        if (!scopedProcessesSnapshot.isValidHandle())
+            return false;
 
-    std::wstring procName;
-    for (BOOL bok = Process32First(processesSnapshot, &processInfo); bok; bok = Process32Next(processesSnapshot, &processInfo))
-    {
-        if (aPid == processInfo.th32ProcessID)
+        HANDLE processesSnapshot = scopedProcessesSnapshot.getHandle();
+        for (BOOL bok = Process32First(processesSnapshot, &processInfo); bok; bok = Process32Next(processesSnapshot, &processInfo))
         {
-            CloseHandle(processesSnapshot);
-            procName = std::wstring(processInfo.szExeFile);
-            break;
+            if (aPid == processInfo.th32ProcessID)
+            {
+                CloseHandle(processesSnapshot);
+                procName = std::wstring(processInfo.szExeFile);
+                break;
+            }
         }
     }
-    CloseHandle(processesSnapshot);
 
     *out = procName;
     return !procName.empty();
 }
 
 //---------------------------------------------------------------------------------------------------------------------------
-class LockInfo
+namespace
 {
-    PVOID _address;
-    HANDLE _owningThd;
-    HANDLE _lockingSem;
-    int _contentionCount;
-    long _lockCountRaw;
-    int _type;
-    std::vector<PVOID> _creationTraceback;
-
-public:
-    LockInfo(HANDLE hProc, PRTL_DEBUG_INFORMATION buffer, size_t idx)
+    class LockInfo
     {
-        const RTL_PROCESS_LOCK_INFORMATION& info = buffer->Locks->Locks[idx];
+        PVOID _address;
+        HANDLE _owningThd;
+        HANDLE _lockingSem;
+        int _contentionCount;
+        int _entryCount;
+        int _recursionCount;
+        long _lockCountRaw;
+        int _type;
+        std::vector<PVOID> _creationTraceback;
 
-        _address = info.Address;
-        _owningThd = info.OwningThread;
-        _contentionCount = info.ContentionCount;
-        _lockCountRaw = info.LockCount;
-        _type = info.Type;
-        _lockingSem = NULL;
-
-        //	CS is from other process' memory- do a process memory read
-        //	else this will lead to access violation
-        //
-        RTL_CRITICAL_SECTION _cs;
-        size_t numBytesRead = 0;
-        BOOL result = ReadProcessMemory(hProc, _address, &_cs, sizeof(_cs), &numBytesRead);
-        if (result)
+    public:
+        LockInfo(HANDLE hProc, PRTL_DEBUG_INFORMATION buffer, size_t idx)
         {
-            _owningThd = _cs.OwningThread;
-            _lockingSem = _cs.LockSemaphore;
-        }
+            const RTL_PROCESS_LOCK_INFORMATION& info = buffer->Locks->Locks[idx];
 
-        //	in case buffer has traceback info, leap of faith really
-        if (buffer->BackTraces != nullptr && buffer->BackTraces->BackTraces != nullptr)
-        {
-            const RTL_PROCESS_BACKTRACE_INFORMATION& tb = buffer->BackTraces->BackTraces[info.CreatorBackTraceIndex];
-            for (int idx = 0; idx < MAX_STACK_DEPTH; ++idx)
+            _address = info.Address;
+            _owningThd = info.OwningThread;
+            _contentionCount = info.ContentionCount;
+            _entryCount = info.EntryCount;
+            _recursionCount = info.RecursionCount;
+            _lockCountRaw = info.LockCount;
+            _type = info.Type;
+            _lockingSem = NULL;
+
+            //	CS is from other process' memory- do a process memory read
+            //	else this will lead to access violation
+            //
+            RTL_CRITICAL_SECTION _cs;
+            size_t numBytesRead = 0;
+            BOOL result = ReadProcessMemory(hProc, _address, &_cs, sizeof(_cs), &numBytesRead);
+            if (result)
             {
-                if (tb.BackTrace[idx] == nullptr)
-                    break;
-                else
-                    _creationTraceback.push_back(tb.BackTrace[idx]);
+                _owningThd = _cs.OwningThread;
+                _lockingSem = _cs.LockSemaphore;
+            }
+
+            //	in case buffer has traceback info, leap of faith really
+            if (buffer->BackTraces != nullptr && buffer->BackTraces->BackTraces != nullptr)
+            {
+                const RTL_PROCESS_BACKTRACE_INFORMATION& tb = buffer->BackTraces->BackTraces[info.CreatorBackTraceIndex];
+                for (int idx = 0; idx < MAX_STACK_DEPTH; ++idx)
+                {
+                    if (tb.BackTrace[idx] == nullptr)
+                        break;
+                    else
+                        _creationTraceback.push_back(tb.BackTrace[idx]);
+                }
             }
         }
-    }
 
-    PVOID address() const
-    {
-        return _address;
-    }
+        PVOID address() const
+        {
+            return _address;
+        }
 
-    int type() const
-    {
-        return _type;
-    }
+        int type() const
+        {
+            return _type;
+        }
 
-    HANDLE owningThread() const
-    {
-        return _owningThd;
-    }
+        HANDLE owningThread() const
+        {
+            return _owningThd;
+        }
 
-    int contentionCount() const
-    {
-        return _contentionCount;
-    }
+        int contentionCount() const
+        {
+            return _contentionCount;
+        }
 
-    long lockCountRaw() const
-    {
-        //	https://msdn.microsoft.com/en-us/library/ff541979(v=vs.85).aspx
-        //	rules:
-        //	1) 0th bit shows lock status
-        //	2) 1st bit shows whether thread has been woken for this
-        //	3) remaining bits are 1s complement for #threads waiting for this lock
-        //
-        return _lockCountRaw;
-    }
+        //  a high entry count -- means CS is going undergoing lots of contention
+        //  this field is always incremented and never decremented
+        int entryCount() const
+        {
+            return _entryCount;
+        }
 
-    bool isLocked() const
-    {
-        return (_lockCountRaw & 0x1) == 0;
-    }
+        int recursionCount() const
+        {
+            return _recursionCount;
+        }
 
-    bool hasWokenThreads() const
-    {
-        return ((_lockCountRaw & 0x2) >> 1 == 0);
-    }
+        long lockCountRaw() const
+        {
+            //	https://msdn.microsoft.com/en-us/library/ff541979(v=vs.85).aspx
+            //	rules:
+            //	1) 0th bit shows lock status
+            //	2) 1st bit shows whether thread has been woken for this
+            //	3) remaining bits are 1s complement for #threads waiting for this lock
+            //
+            return _lockCountRaw;
+        }
 
-    int numThreadsWaiting() const
-    {
-        return (-1 - _lockCountRaw) >> 2;
-    }
+        bool isLocked() const
+        {
+            return (_lockCountRaw & 0x1) == 0;
+        }
 
-    bool hasCreationTraceback() const
-    {
-        return !_creationTraceback.empty();
-    }
+        bool hasWokenThreads() const
+        {
+            return ((_lockCountRaw & 0x2) >> 1 == 0);
+        }
 
-    std::vector<PVOID> creationTraceback() const
-    {
-        return _creationTraceback;
-    }
+        int numThreadsWaiting() const
+        {
+            return (-1 - _lockCountRaw) >> 2;
+        }
 
-    bool hasLockingSemaphore() const
-    {
-        return _lockingSem;
-    }
+        bool hasCreationTraceback() const
+        {
+            return !_creationTraceback.empty();
+        }
 
-    HANDLE lockingSemaphore() const
+        std::vector<PVOID> creationTraceback() const
+        {
+            return _creationTraceback;
+        }
+
+        bool hasLockingSemaphore() const
+        {
+            return _lockingSem;
+        }
+
+        HANDLE lockingSemaphore() const
+        {
+            return _lockingSem;
+        }
+
+        bool isOwningThreadAlive() const
+        {
+            return true;
+        }
+    };
+
+    class LocksInfoComparator_ContentionCount
     {
-        return _lockingSem;
-    }
-};
+    public:
+        LocksInfoComparator_ContentionCount() = default;
+        ~LocksInfoComparator_ContentionCount() = default;
+
+        bool operator ()(const LockInfo& first, const LockInfo& second)
+        {
+            return first.contentionCount() < second.contentionCount();
+        }
+    };
+}
 
 //---------------------------------------------------------------------------------------------------------------------------
 static void printLockInfo(size_t idx, const LockInfo& lockInfo)
@@ -184,6 +253,8 @@ static void printLockInfo(size_t idx, const LockInfo& lockInfo)
     _tprintf(L"OwningThread:      %d\n", lockInfo.owningThread());
     _tprintf(L"LockingSemaphore:  0x%p\n", lockInfo.lockingSemaphore());
     _tprintf(L"ContentionCount:   %lu\n", lockInfo.contentionCount());
+    _tprintf(L"EntryCount:        %lu\n", lockInfo.entryCount());
+    _tprintf(L"RecursionCount:    %lu\n", lockInfo.recursionCount());
     _tprintf(L"LockCountRaw:      %ld\n", lockInfo.lockCountRaw());
     _tprintf(L"  LockStatus:      %ls\n", (lockInfo.isLocked() ? L"locked" : L"not-locked"));
     _tprintf(L"  AnyThreadsWoken: %ls\n", (lockInfo.hasWokenThreads() ? L"yes" : L"no"));
@@ -226,9 +297,10 @@ int wmain(int argc, WCHAR* argv[])
 
     std::vector<LockInfo> lockInfoVec;
 
-    HANDLE currProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-    if (currProc != NULL && currProc != INVALID_HANDLE_VALUE)
+    ScopedHandle scopedCurrProcHdl(OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid));
+    if (scopedCurrProcHdl.isValidHandle())
     {
+        HANDLE currProc = scopedCurrProcHdl.getHandle();
         PRTL_DEBUG_INFORMATION buffer = pfnRtlCreateQueryDebugBuffer(0, 0);
         if (buffer != NULL)
         {
@@ -246,7 +318,6 @@ int wmain(int argc, WCHAR* argv[])
             status = pfnRtlDestroyQueryDebugBuffer(buffer);
             buffer = nullptr;
         }
-        CloseHandle(currProc);
 
         if (!lockInfoVec.empty())
         {
@@ -255,7 +326,8 @@ int wmain(int argc, WCHAR* argv[])
 
             std::vector<LockInfo> acquiredLocks;
             std::vector<LockInfo> unLockedLocks;
-
+            std::set<LockInfo, LocksInfoComparator_ContentionCount> contentiousLocks;
+            std::vector<LockInfo> abandonedLocks;
 
             for (const LockInfo& lk : lockInfoVec)
             {
@@ -263,7 +335,24 @@ int wmain(int argc, WCHAR* argv[])
                     acquiredLocks.push_back(lk);
                 else
                     unLockedLocks.push_back(lk);
+
+                if(lk.contentionCount() > 2)
+                    contentiousLocks.insert(lk);
+
+                if (!lk.isOwningThreadAlive())
+                    abandonedLocks.push_back(lk);
             }
+
+            if (!abandonedLocks.empty())
+            {
+                _tprintf(L"\nPrinting %zu abandoned locks\n", abandonedLocks.size());
+                _tprintf(L"------------------------------------------------------------------------\n");
+                for (size_t idx = 0; idx < abandonedLocks.size(); ++idx)
+                    printLockInfo(idx, abandonedLocks[idx]);
+
+                _tprintf(L"\n\n");
+            }
+
 
             if (!acquiredLocks.empty())
             {
@@ -271,6 +360,19 @@ int wmain(int argc, WCHAR* argv[])
                 _tprintf(L"------------------------------------------------------------------------\n");
                 for (size_t idx = 0; idx < acquiredLocks.size(); ++idx)
                     printLockInfo(idx, acquiredLocks[idx]);
+
+                _tprintf(L"\n\n");
+            }
+
+            if (!contentiousLocks.empty())
+            {
+                int counter = 0;
+                _tprintf(L"\nPrinting %zu most contentious locks\n", contentiousLocks.size());
+                _tprintf(L"------------------------------------------------------------------------\n");
+                for (const LockInfo& lockInfo : contentiousLocks)
+                    printLockInfo(counter++, lockInfo);
+
+                _tprintf(L"\n\n");
             }
 
             bool extraDbg = false;
